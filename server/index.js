fcc453029a81a3a0baae53f62f1f46f6f9f67a57
Guy Hazut <guyhazut2000@gmail.com>
2021-12-26 15:57:35 +0200

    test

--- server/index.js
+++ server/index.js
@@ -17,11 +17,13 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoute);
-app.use("/api/car-treatments", carTreatmentRoute);
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+// app.use("/api/users", userRoute);
+// app.use("/api/car-treatments", carTreatmentRoute);
+app.use("/users", userRoute);
+app.use("/car-treatments", carTreatmentRoute);
+app.use("*", (req, res) => res.status(404).json({ error: "page not found" }));
 
 //start server
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+  console.log("Backend server is running on port " + process.env.PORT);
 });
