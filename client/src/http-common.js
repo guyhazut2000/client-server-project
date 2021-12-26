2f93a363f715f377d3edeae4c3c34881e7ccfc86
Guy Hazut <guyhazut2000@gmail.com>
2021-12-26 16:59:47 +0200

    ""

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "client-server-2021-project-app.herokuapp.com:5000/api",
   headers: {
     "Content-type": "application/json",
   },
