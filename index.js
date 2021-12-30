91fdacd02185e6c7a33f0aadb209e00309288de0
Guy Hazut <guyhazut2000@gmail.com>
2021-12-30 16:16:26 +0200

    deploy

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ dotenv.config();
 const cors = require("cors");
 const userRoute = require("./routes/user");
 const carTreatmentRoute = require("./routes/carTreatment");
+const path = require("path");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -20,6 +21,15 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/car-treatments", carTreatmentRoute);
 app.use("*", (req, res) => res.status(404).json({ error: "Page not found" }));
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("/client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //start server
 app.listen(process.env.PORT || 5000, () => {
