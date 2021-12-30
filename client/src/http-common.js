f3399642f36196ade4c0a2e78a5f57c5eda72456
Guy Hazut <guyhazut2000@gmail.com>
2021-12-30 16:36:27 +0200

    test

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://stark-harbor-89554/api.herokuapp.com",
   headers: {
     "Content-type": "application/json",
   },
